ba9e7d7189e4f9f4b62d69cf647bdcd9fff264d7
Bret Cameron <bretcameron@gmail.com>
2026-02-14 10:10:02 +0000

    Add CSP with nonce for any scripted webview

--- src/html-builder.ts
+++ src/html-builder.ts
@@ -140,5 +140,10 @@ export async function buildHtml(
 }
 
 export function escapeHtml(s: string): string {
-  return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  return s
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
